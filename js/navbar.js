a02517e86f0e2a735b12c16f42b3d47a130e61f6
Joffrey Sterckx <147731791+SterckxJoffrey@users.noreply.github.com>
2026-03-03 13:37:26 +0100

    Nav bar file

---
+++ js/navbar.js
@@ -0,0 +1,8 @@
+const navContainer = document.getElementById("nav");
+console.log(navContainer)
+
+fetch('/pages/nav.html')
+  .then(response => response.text())
+  .then(data => {
+    navContainer.innerHTML = data; 
+  });
