194a9ab23a83d963e2ff95c290e3805f1a6598d6
soddi05 <soddi@student.efp.be>
2026-03-03 16:12:05 +0100

    Version 2.0

--- js/navbar.js
+++ js/navbar.js
@@ -1,8 +1,22 @@
 const navContainer = document.getElementById("nav");
-console.log(navContainer)
 
-fetch('/pages/nav.html')
+fetch('pages/nav.html')
   .then(response => response.text())
   .then(data => {
-    navContainer.innerHTML = data; 
+    navContainer.innerHTML = data;
+    
+    const sidenav = document.getElementById("mySidenav");
+    const openBtn = document.getElementById("openBtn");
+    const closeBtn = document.getElementById("closeBtn");
+    
+    openBtn.onclick = (e) => {
+      e.preventDefault();
+      sidenav.classList.add("active");
+      openBtn.classList.add("hidden");
+    };
+    closeBtn.onclick = (e) => {
+      e.preventDefault();
+      sidenav.classList.remove("active");
+      openBtn.classList.remove("hidden");
+    };
   });
