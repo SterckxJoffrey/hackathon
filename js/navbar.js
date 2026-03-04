20c1689af8795ad77a446511592de5d446b8f7d2
Joffrey Sterckx <147731791+SterckxJoffrey@users.noreply.github.com>
2026-03-04 22:16:49 +0100

    concert nav bar

--- js/navbar.js
+++ js/navbar.js
@@ -1,14 +1,14 @@
   const links = document.querySelectorAll('.navbar a');
 
-  // Récupère le nom du fichier de la page actuelle
-  const currentPage = window.location.pathname.split('/').pop(); // ex: "waiting.html"
+  
+  const currentPage = window.location.pathname.split('/').pop(); 
 
   links.forEach(link => {
-    // Récupère le href du lien
+ 
     const linkPage = link.getAttribute('href');
 
-    // Si le href correspond au nom du fichier actuel
+
     if (linkPage === currentPage) {
-      link.classList.add('active'); // ajoute la classe "active"
+      link.classList.add('active'); 
     }
   });
