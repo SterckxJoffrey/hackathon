eb6abf0bc02a8ccf7d62366d21acbc15f5d2e83e
Joffrey Sterckx <147731791+SterckxJoffrey@users.noreply.github.com>
2026-03-04 19:10:59 +0100

    Test song deploy

--- js/song.js
+++ js/song.js
@@ -1,14 +1,14 @@
-const spotifyEmbeds = [
-  `<iframe data-testid="embed-iframe" style="border-radius:12px" src="https://open.spotify.com/embed/track/3AyeEQHxCOzJ22b2mQTUPK?utm_source=generator" width="90%" height="152" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>`,
-  `<iframe data-testid="embed-iframe" style="border-radius:12px" src="https://open.spotify.com/embed/track/7mbjbYsq0gFOD6qm9oOlVT?utm_source=generator" width="90%" height="152" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>`,
-  `<iframe data-testid="embed-iframe" style="border-radius:12px" src="https://open.spotify.com/embed/track/5Y8lc1PFBvS4a6yoLBCocW?utm_source=generator" width="90%" height="152" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>`,
-  '<iframe data-testid="embed-iframe" style="border-radius:12px" src="https://open.spotify.com/embed/track/1t2DuK4AAnNbaiQl7QKFKA?utm_source=generator" width="90%" height="152" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>'
-];
+// const spotifyEmbeds = [
+//   `<iframe data-testid="embed-iframe" style="border-radius:12px" src="https://open.spotify.com/embed/track/3AyeEQHxCOzJ22b2mQTUPK?utm_source=generator" width="90%" height="152" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>`,
+//   `<iframe data-testid="embed-iframe" style="border-radius:12px" src="https://open.spotify.com/embed/track/7mbjbYsq0gFOD6qm9oOlVT?utm_source=generator" width="90%" height="152" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>`,
+//   `<iframe data-testid="embed-iframe" style="border-radius:12px" src="https://open.spotify.com/embed/track/5Y8lc1PFBvS4a6yoLBCocW?utm_source=generator" width="90%" height="152" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>`,
+//   '<iframe data-testid="embed-iframe" style="border-radius:12px" src="https://open.spotify.com/embed/track/1t2DuK4AAnNbaiQl7QKFKA?utm_source=generator" width="90%" height="152" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>'
+// ];
 
-const container = document.getElementById("songsContainer");
+// const container = document.getElementById("songsContainer");
 
-spotifyEmbeds.forEach(embedCode => {
-  const wrapper = document.createElement("div");
-  wrapper.innerHTML = embedCode;
-  container.appendChild(wrapper);
-});
+// spotifyEmbeds.forEach(embedCode => {
+//   const wrapper = document.createElement("div");
+//   wrapper.innerHTML = embedCode;
+//   container.appendChild(wrapper);
+// });
