d171916569eedb0b3f27f0b0152599d14d247fec
Joffrey Sterckx <147731791+SterckxJoffrey@users.noreply.github.com>
2026-03-04 23:17:20 +0100

    Home page artiste name +media queries

--- js/animation.js
+++ js/animation.js
@@ -1,11 +1,11 @@
 
-// const h1 = document.getElementById('animated-text');
-// const text = h1.textContent;
-// h1.textContent = '';
+const h1 = document.getElementById('artist-name');
+const text = h1.textContent;
+h1.textContent = '';
 
-// text.split('').forEach((char, i) => {
-//   const span = document.createElement('span');
-//   span.textContent = char === ' ' ? '\u00A0' : char;
-//   span.style.animationDelay = `${i * 0.08}s`; 
-//   h1.appendChild(span);
-// });
+text.split('').forEach((char, i) => {
+  const span = document.createElement('span');
+  span.textContent = char === ' ' ? '\u00A0' : char;
+  span.style.animationDelay = `${i * 0.08}s`; 
+  h1.appendChild(span);
+});
