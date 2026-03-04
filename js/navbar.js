ef56864bcdff586cfa1633ebbe5807ae39fa384f
shadow7381606 <clinetchase@gmail.com>
2026-03-04 18:11:42 +0100

    formulaire finish

--- js/navbar.js
+++
@@ -1,8 +0,0 @@
-const navContainer = document.getElementById("nav");
-console.log(navContainer)
-
-fetch('/pages/nav.html')
-  .then(response => response.text())
-  .then(data => {
-    navContainer.innerHTML = data; 
-  });
