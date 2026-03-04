ef499cdabb00ceab6acf80e7356af85b6e174ce2
Joffrey Sterckx <147731791+SterckxJoffrey@users.noreply.github.com>
2026-03-04 18:51:03 +0100

    Song pages

--- js/song.js
+++ js/song.js
@@ -1,16 +1,14 @@
-// concert.js
-
-// Liste des iframes Spotify embed (tu peux en ajouter autant que tu veux)
 const spotifyEmbeds = [
-  `<iframe data-testid="embed-iframe" style="border-radius:12px" src="https://open.spotify.com/embed/track/3AyeEQHxCOzJ22b2mQTUPK?utm_source=generator&theme=0" width="100%" height="152" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>`,
-  `<iframe data-testid="embed-iframe" style="border-radius:12px" src="https://open.spotify.com/embed/track/1rgnBhdG2JDFTbYkYRZAku?utm_source=generator&theme=0" width="100%" height="152" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>`,
-  `<iframe data-testid="embed-iframe" style="border-radius:12px" src="https://open.spotify.com/embed/track/5ChkMS8OtdzJeqyybCc9R5?utm_source=generator&theme=0" width="100%" height="152" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>`
+  `<iframe data-testid="embed-iframe" style="border-radius:12px" src="https://open.spotify.com/embed/track/3AyeEQHxCOzJ22b2mQTUPK?utm_source=generator" width="90%" height="152" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>`,
+  `<iframe data-testid="embed-iframe" style="border-radius:12px" src="https://open.spotify.com/embed/track/7mbjbYsq0gFOD6qm9oOlVT?utm_source=generator" width="90%" height="152" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>`,
+  `<iframe data-testid="embed-iframe" style="border-radius:12px" src="https://open.spotify.com/embed/track/5Y8lc1PFBvS4a6yoLBCocW?utm_source=generator" width="90%" height="152" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>`,
+  '<iframe data-testid="embed-iframe" style="border-radius:12px" src="https://open.spotify.com/embed/track/1t2DuK4AAnNbaiQl7QKFKA?utm_source=generator" width="90%" height="152" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>'
 ];
 
-// Récupération du container
 const container = document.getElementById("songsContainer");
 
-// Ajout de chaque iframe dans le container
 spotifyEmbeds.forEach(embedCode => {
-  container.innerHTML += embedCode + "<br>"; // <br> pour un petit espace entre les musiques
+  const wrapper = document.createElement("div");
+  wrapper.innerHTML = embedCode;
+  container.appendChild(wrapper);
 });
